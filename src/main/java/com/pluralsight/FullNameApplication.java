2855104617be33c8fef7d45a2a91c672ce484b9e
Ciara <lajonllc@gmail.com>
2024-09-30 15:55:36 -0500

    First commit

---
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class FullNameApplication {
+    public static void FullNameApplication(String[]args){
+        Scanner scanner = new scaner(System.in);
+
+        System.out.print = ("Enter your first name");
+        String firstname = scanner.nextLine();
+
+        System.out.print("Enter your middle name (press Enter to skip: ");
+        String middlename=scanner.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastname = scanner.nextLine();
+
+        System.out.print("Enter your suffix (press Enter to skip:");
+        String suffix = scanner.nextLine();
+
+
+
+    }
+}
